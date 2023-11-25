93aa1a04467b58a66ae20ca30d093ca0804c7960
Cássio Fellipe <cassio.fellipeff@gmail.com>
2023-11-25 09:03:33 -0300

    Trabalhando no Bcrypt

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,8 +1,18 @@
 export const signup = (app) => {
-    app.post('/users/signup', (requisicao, resposta) => {
-        console.log(requisicao.body)
+    app.get('/users', (req, res) => { 
+        res.json(users)
+    });
 
-        resposta.send(requisicao.body)
+    app.post('/users', async (req, res) => {
+        try{
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const user = {name: req.body.name, password: hashedPassword};
+            users.push(user);
+            res.status(201).send();
+        } catch {
+            res.status(500).send();
+        }
     })
 }
 
+const users = [];
